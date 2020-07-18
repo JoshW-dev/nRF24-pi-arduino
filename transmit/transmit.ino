aedcb3a84da64b1090c4edcf79c9161fbae9a829
Josh Wade <joshuafwade@gmail.com>
2020-07-18 16:17:31 -0400

    initial commit

---
+++ transmit/transmit.ino
@@ -0,0 +1,20 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+String request;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("transmitter");
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {
+  const char text[] = "It's working";
+  radio.write(&text, sizeof(text));
+  delay(1000);
+}
